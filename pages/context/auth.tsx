0a16217feaa670cb2ebd6a8320f261d6a093be46
wistell <wistera.mono@gmail.com>
2024-08-18 22:56:19 +0900

    done

---
+++ pages/context/auth.tsx
@@ -0,0 +1,78 @@
+import React, { ReactNode, useContext, useEffect, useState } from "react";
+import apiClient from "../lib/apiClient";
+
+interface AuthContextType {
+    user: null | {
+        id: Number;
+        email: string; 
+        username: string;
+    };
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+const AuthContext = React.createContext<AuthContextType>({
+    user: null,
+    login: () => {},
+    logout: () => {},
+});
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [user, setUser] = useState<null | {
+        id: Number;
+        email: string; 
+        username: string;
+    }>(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem("auth_token");
+        if(token) {
+            apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;    
+
+            apiClient.get("/users/find").then((res) => {
+                setUser(res.data.user);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }, []);
+
+    const login = async (token: string) => {
+        localStorage.setItem("auth_token", token);
+        apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+        try {
+            apiClient.get("/users/find").then((res) => {
+                setUser(res.data.user);
+            });
+        } catch (error) {
+            
+        }
+    };
+
+    const logout = () => {
+        localStorage.removeItem("auth_token");
+        delete apiClient.defaults.headers["Authorization"];
+        setUser(null);
+    };  
+
+    const value = {
+        user,
+        login,
+        logout,
+    };
+
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
---
+++ pages/types.ts
@@ -0,0 +1,25 @@
+export interface Profile {
+  id: number;
+  bio: string;
+  profileImageUrl: string;
+  userId: number;
+  user: UserType;
+};
+
+export interface UserType {
+    id: number;
+    username: String;    
+    email:      String;
+    password:   String;
+    posts:      PostType[];
+    profile:    Profile;
+};
+
+export interface PostType {
+  id: number;
+  content: string;
+  createdAt: string;
+  authorId: number;
+  author: UserType;
+};
+
